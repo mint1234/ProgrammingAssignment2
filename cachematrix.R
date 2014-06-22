178070ba72cdac3862885eb49edc8800863e528d
mint1234 <tumin.ly@hotmail.com>
2014-06-22 14:51:14 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,18 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inverse <- NULL
+  set <- function(y){
+    x <<- y
+    inverse <<- NULL
+  }
+  get <- function()x
+  setinverse <- function(inversed) inverse <<- inversed
+  getinverse <- function()inverse
+  list(set = set,get = get,
+       setinverse = setinverse,
+       getinverse = getinverse
+    )
 }
 
 
@@ -12,4 +23,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inverse <- x$getinverse()
+    if(!is.null(inverse)){
+    message("Getting the cached data")
+    return (inverse)
+  }
+  data <- x$get()
+  inverse <- inversed(data,...)
+  x$setinverse(inverse)
+  inverse
 }
